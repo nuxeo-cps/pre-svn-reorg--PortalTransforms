211a4ac5939431855eb876f72ed4f7bf52956b00
rspivak <devnull@localhost>
2005-11-30 15:44:49 +0000

    Added support for xsl_to_html on win32. Step forward to fix #708

--- transforms/xls_to_html.py
+++ transforms/xls_to_html.py
@@ -21,11 +21,14 @@
 """
 
 """
+import os
+import sys
+
 from Products.PortalTransforms.interfaces import itransform
 from Products.PortalTransforms.libtransforms.utils import basename, sansext
 from Products.PortalTransforms.libtransforms.commandtransform \
     import commandtransform
-from os import system
+
 
 class xls_to_html(commandtransform):
     __implements__ = itransform
@@ -55,18 +58,25 @@ class xls_to_html(commandtransform):
         return cache
 
     def invokeCommand(self, tmpdir, fullname):
-        # FIXME: windows users...
         basename = sansext(fullname)
-        cmd = 'cd "%s" && %s %s "%s" 1> "%s.html" 2>error_log' % (
-            tmpdir, self.binary, self.binaryArgs, fullname, basename)
-        system(cmd)
+        if sys.platform == 'win32':
+            cmd = '%s %s "%s" > "%s.html" 2>"%s"' % (
+                self.binary,
+                self.binaryArgs,
+                fullname,
+                os.path.join(tmpdir, basename),
+                os.path.join(tmpdir, 'error_log'))
+        else:
+            cmd = 'cd "%s" && %s %s "%s" 1> "%s.html" 2>error_log' % (
+                tmpdir, self.binary, self.binaryArgs, fullname, basename)
+        os.system(cmd)
         try:
-            htmlfile = open("%s/%s.html" % (tmpdir, basename), 'r')
+            htmlfile = open(os.path.join(tmpdir, basename+'.html'))
             html = htmlfile.read()
             htmlfile.close()
         except:
             try:
-                return open("%s/error_log" % tmpdir, 'r').read()
+                return open(os.path.join(tmpdir, 'error_log')).read()
             except:
                 return ''
         return html
